a0ef4f0fc890c3be6bbecbe9c1f4a8aee259790e
Gilad Glasberg <giladg@tikalk.com>
2017-07-10 11:19:54 +0300

    mongodb db on mongolab connect

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,33 @@
  * Created by robertferentz on 10/07/17.
  */
 import * as express from 'express'
+import * as mongo from 'mongodb'
+const mongoDb = mongo.MongoClient;
+const url = "mongodb://tikal:123123@ds153682.mlab.com:53682/tikal-roadmap";
+
 import * as bodyParser from 'body-parser'
 const port: number = 8080
 
+const app = express();
+
+app.listen(port);
 const app = express()
+
+mongoDb.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+// mongoDb.connect(url, function(err, db) {
+//     if (err) throw err;
+//     const myobj = { name: "Company Inc", address: "Highway 37" };
+//     db.collection("customers").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("1 record inserted");
+//         db.close();
+//     });
+// });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
