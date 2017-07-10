416eb2be958a9e344bdc65595dc1a9c985fbedcc
Robert Ferentz <RobiFerentz@gmail.com>
2017-07-10 11:38:50 +0300

    fixes

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/mongodb/index.d.ts" />
 /// <reference path="modules/winston/index.d.ts" />
