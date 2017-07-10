9eff012d8206372dc9e40ca6c34be084ba0139bf
Robert Ferentz <RobiFerentz@gmail.com>
2017-07-10 13:22:06 +0300

    moved creation of roadmap item to roadmap routes

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@
  */
 import * as express from 'express'
 import rmRouter from './routes/RoadmapRoutes'
-import {Roadmap} from './db/models/roadmap.model';
+
 
 import * as bodyParser from 'body-parser'
-import * as _ from 'lodash'
 
 import * as winston from 'winston'
 const port: number = 8080
@@ -21,32 +20,6 @@ app.use(bodyParser.json())
 
 app.get('/', (req:express.Request, res: express.Response) => {
     res.send('Hello world!')
-})
-
-app.post('/createItem', function (req, res, next){
-    const item = _.get(req, 'req.body.item', '')
-    if (!item) {
-        console.log('request from client did not arrive correctly')
-        res.status(404).send('Error, request from client not valid')
-    }
-    const roadmapItem = new Roadmap({
-        title: String,
-        stepResources: String,
-        exercise: String,
-        testFile: String,
-        version: String,
-        author: String,
-        isActive: Boolean
-    });
-    roadmapItem.save(function (err) {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Error on mongo request')
-        } else {
-            console.log('item added to roadmap collection on mongo');
-            res.status(201).send('Item successfully added to mongo')
-        }
-    });
 })
 
 app.use('/roadmaps/', rmRouter)
--- src/routes/RoadmapRoutes.ts
+++ src/routes/RoadmapRoutes.ts
@@ -2,6 +2,8 @@
  * Created by robertferentz on 10/07/17.
  */
 import { Router } from 'express'
+import {Roadmap} from '../db/models/roadmap.model';
+import * as _ from 'lodash'
 
 class RoadmapRoutes {
     router: Router
@@ -23,9 +25,29 @@ class RoadmapRoutes {
         })
 
         this.router.post('/', (req, res) => {
-            res.json({
-                msg:'Road map added'
-            })
+            const item = _.get(req, 'req.body.item', '')
+            if (!item) {
+                console.log('request from client did not arrive correctly')
+                res.status(404).send('Error, request from client not valid')
+            }
+            const roadmapItem = new Roadmap({
+                title: String,
+                stepResources: String,
+                exercise: String,
+                testFile: String,
+                version: String,
+                author: String,
+                isActive: Boolean
+            });
+            roadmapItem.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Error on mongo request')
+                } else {
+                    console.log('item added to roadmap collection on mongo');
+                    res.status(201).send('Item successfully added to mongo')
+                }
+            });
         })
 
         this.router.delete('/', (req, res) => {
