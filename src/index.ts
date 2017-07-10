fd2aad9430142cc47cf0e7259228bbb6547d17b3
Robert Ferentz <RobiFerentz@gmail.com>
2017-07-10 11:12:58 +0300

    added body parser

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@
  * Created by robertferentz on 10/07/17.
  */
 import * as express from 'express'
-
+import * as bodyParser from 'body-parser'
 const port: number = 8080
-
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.listen(port)
 
-app.get('/', (req, res)=> {
+app.get('/', (req:express.Request, res: express.Response) => {
     res.send('Hello world!')
 })
 
